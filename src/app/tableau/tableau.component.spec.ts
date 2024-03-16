344191e9ee59034c6db0a56ad49486e6be5ce903
Andrew Ferrin <andjferrin@gmail.com>
2024-03-16 16:23:27 -0400

    Update TableauComponent Test to pass

--- src/app/tableau/tableau.component.spec.ts
+++ src/app/tableau/tableau.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { RouterTestingModule } from "@angular/router/testing";
 
 import { TableauComponent } from "./tableau.component";
 
@@ -8,9 +10,9 @@ describe("TableauComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TableauComponent]
-        })
-            .compileComponents();
+            imports: [TableauComponent, RouterTestingModule],
+            providers: [provideAnimationsAsync()]
+        }).compileComponents();
 
         fixture = TestBed.createComponent(TableauComponent);
         component = fixture.componentInstance;
